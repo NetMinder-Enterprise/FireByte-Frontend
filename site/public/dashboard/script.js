73694364de5da2a04730d07a287b1ff5a3476b96
Nicolly Juliani <nicollyjuliane2004@gmail.com>
2023-10-11 16:49:34 -0300

    legenda + botão ordenar

--- site/public/dashboard/script.js
+++ site/public/dashboard/script.js
@@ -85,8 +85,68 @@ function updateProgressBar(percentage, fillId, percentageId) {
   
 
 
+/*imagem*/
+
+const uploadButton = document.getElementById('uploadButton');
+const fileInput = document.getElementById('fileInput');
+const userImage = document.getElementById('userImage');
+const imageModal = document.getElementById('imageModal');
+const closeModal = document.getElementById('closeModal');
+const modalImage = document.getElementById('modalImage');
+const removeButton = document.getElementById('removeButton');
+const replaceButton = document.getElementById('replaceButton');
+
+// Abrir a modal ao clicar no botão "Carregar Imagem"
+uploadButton.addEventListener('click', function () {
+  imageModal.style.display = 'block';
+  modalImage.src = userImage.src;
+});
+
+// Fechar a modal ao clicar no "X"
+closeModal.addEventListener('click', function () {
+  imageModal.style.display = 'none';
+});
+
+// Remover a imagem ao clicar no botão "Remover"
+removeButton.addEventListener('click', function () {
+  userImage.src = 'default.jpg';
+  imageModal.style.display = 'none';
+});
+
+// Adicionar um ouvinte de eventos ao elemento input de arquivo
+fileInput.addEventListener('change', function () {
+  const file = fileInput.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      userImage.src = e.target.result;
+      modalImage.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+  imageModal.style.display = 'none';
+});
+
+// Trocar a imagem ao clicar no botão "Trocar"
+replaceButton.addEventListener('click', function () {
+  fileInput.click();
+});
 
 
+
+
+
+
+
+
+
+
+
+  
 
 
 
