11c1d10fd5a39d146bc29fcc3b8c0d664bac231b
KevinDSWesselka <kevin.wesselka@sptech.school>
2023-11-26 16:17:37 -0300

    @KevinDSWesselka -Ajuste em notificacoesToastr

--- site/public/js/notificacoesToastr.js
+++ site/public/js/notificacoesToastr.js
@@ -1,49 +1,32 @@
-        var usoCPU = 85;
-        var usoRAM = 80;
-        var usoDISCO = 98;
-        var usoREDE = 97;
-        var componente = "CPU"
-        function showNotification() {
-            var notificationBuilder = [
-            //Interesante pensar em algo que não dependa de uma URL!!!
-            //Tambem vamos ter de correr essa lista pra resgatar dados do banco...
-                { nome: componente, uso: usoCPU, dispositivo: "1", url: "../dashboard/dispositivos.html" },
-                { nome: componente, uso: usoRAM, dispositivo: "2", url: "../dashboard/dispositivos.html" },
-                { nome: componente, uso: usoDISCO, dispositivo: "3", url: "../dashboard/dispositivos.html" },
-                { nome: componente, uso: usoREDE, dispositivo: "4", url: "../dashboard/dispositivos.html" }
-            ];
-    
-            for (var i = 0; i < notificationBuilder.length; i++) {
-                var notification = notificationBuilder[i];
-    
-                if (notification.uso > 60) {
-                    setTimeout(function (notification) {
-                        toastr.options = {
-                            "closeButton": true,
-                            "debug": false,
-                            "newestOnTop": false,
-                            "progressBar": true,
-                            "positionClass": "toast-top-right",
-                            "preventDuplicates": false,
-                            "onclick": function () {
-                                // Redirecionar para a página da dashboard do dispositivo
-                                window.location.href = notification.url;
-                            },
-                            "showDuration": "300",
-                            "hideDuration": "1000",
-                            "timeOut": "0",
-                            "extendedTimeOut": "0",
-                            "showEasing": "swing",
-                            "hideEasing": "linear",
-                            "showMethod": "fadeIn",
-                            "hideMethod": "fadeOut"
-                        };
-    
-                        toastr.info("Uso da " + notification.nome + " do dispositivo " + notification.dispositivo + " está em: " + notification.uso + "%.<br><a href='" + notification.url + "'>Visualizar dispositivo</a>");
-                    }, i * 2000, notification);
-                }
-            }
+var componentesNotificados = {};
+
+function showNotification(captura, componente) {
+    if (captura > 0) {
+        if (!componentesNotificados[componente] || captura > componentesNotificados[componente].captura) {
+            toastr.options = {
+                "closeButton": true,
+                "debug": false,
+                "newestOnTop": true,
+                "progressBar": true,
+                "positionClass": "toast-top-right",
+                "onclick": function () {
+                    window.location.href = componentesNotificados[componente].url;
+                },
+                "showDuration": "300",
+                "hideDuration": "1000",
+                "timeOut": "0",
+                "extendedTimeOut": "0",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut"
+            };
+
+            toastr.info("Uso de " + componente + " está em: " + captura + "%");
+            componentesNotificados[componente] = {
+                captura: captura,
+                url: "../dashboard/dispositivos.html" // Substitua pela URL correspondente
+            };
         }
-    
-        // Chamada da função para exibir notificações após 2 segundos
-        setTimeout(showNotification, 2000);
+    }
+}
